753018b9bfcfbb8b663d352fcc54a038269381a7
NickRayan <nickcerron@gmail.com>
2023-10-29 17:17:06 -0500

    apigateway.roleservice: findAll, detail to response fixed

--- apps/apigateway/src/modules/role/services/role.service.ts
+++ apps/apigateway/src/modules/role/services/role.service.ts
@@ -2,7 +2,6 @@ import { ROLES_SERVICE_NAME, RolesServiceClient } from '@app/common';
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { AUTH_SERVICE } from '../../user/constants';
 import { ClientGrpc } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class RoleService implements OnModuleInit {
@@ -13,14 +12,7 @@ export class RoleService implements OnModuleInit {
       this.client.getService<RolesServiceClient>(ROLES_SERVICE_NAME);
   }
   async findAll() {
-    try {
-      const { roles } = await firstValueFrom(this.roleService.findAllRoles({}));
-      console.log(roles);
-      return roles;
-    } catch (error) {
-      console.log(error);
-    }
-    //return this.roleService.findAllRoles({});
+    this.roleService.findAllRoles({});
   }
   findOne(id: string) {
     return this.roleService.findOneRole({ id });
