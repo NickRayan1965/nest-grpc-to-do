3b88c4b2dbba3d05d71d40200cbfae94ae154b42
NickRayan <nickcerron@gmail.com>
2023-10-29 02:26:56 -0500

    TODO: user service with his microservices implemented (findOne)

--- apps/to-do/src/modules/user/services/user.service.ts
+++ apps/to-do/src/modules/user/services/user.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { USERS_SERVICE_NAME, UsersServiceController } from '@app/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { AUTH_SERVICE } from '../constants';
+import { ClientGrpc } from '@nestjs/microservices';
 
 @Injectable()
-export class UserService {}
+export class UserService implements OnModuleInit {
+  private userService: UsersServiceController;
+  constructor(@Inject(AUTH_SERVICE) private readonly client: ClientGrpc) {}
+  onModuleInit() {
+    this.userService =
+      this.client.getService<UsersServiceController>(USERS_SERVICE_NAME);
+  }
+
+  async findOneById(id: string) {
+    return this.userService.findOneUser({ id });
+  }
+}
